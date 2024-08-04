887f1bfa2261f26fee9bb95c3b5116c7620efaed
Yevhen Nahalskyi <ltclmb.dev@gmail.com>
2024-08-04 03:21:15 +0200

    theme -toggle component

--- src/components/app/navigation.tsx
+++ src/components/app/navigation.tsx
@@ -1,12 +1,18 @@
 import Link from 'next/link'
+
+import Logo from '@/components/logo'
+import { ThemeToggle } from '@/components/theme-toggle'
+
 export const Navigation = () => {
   return (
-    <header className="py-4">
+    <header className="py-2">
       <nav className="container max-w-none flex justify-between">
         <Link href="/" className="text-4xl">
-          🙌
+          <Logo />
         </Link>
-        {/* <div className="flex items-center"></div> */}
+        <div className="flex items-center">
+          <ThemeToggle />
+        </div>
       </nav>
     </header>
   )
---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import * as React from 'react'
+import { Icon } from '@/components/icons'
+import { useTheme } from 'next-themes'
+
+import { Button } from '@/components/ui/button'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme()
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant={null} size="icon" onClick={toggleTheme}>
+            <div className="text-xl md:text-2xl rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0">
+              <Icon name="Moon" size="16" />
+            </div>
+            <div className="absolute text-xl md:text-2xl rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100">
+              <Icon name="Sun" size="16" />
+            </div>
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{theme === 'light' ? 'Switch to Dark' : 'Switch to Light'}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
