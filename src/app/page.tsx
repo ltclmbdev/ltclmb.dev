4395f3bf58586c0d064392d44b26529bd7690b3c
Yevhen Nahalskyi <ltclmb.dev@gmail.com>
2024-08-04 03:21:34 +0200

    adjust layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-7xl font-semibold">ltclmb</h1>
+    <main className="flex flex-col items-center justify-center p-24 w-full">
+      <h1 className="text-8xl font-semibold">ltclmb.dev</h1>
+      <h3 className="text-4xl mt-8">is in development 👨🏻‍💻</h3>
     </main>
   )
 }
