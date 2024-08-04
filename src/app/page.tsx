3c072a401771405e91aa1896c176ef225216eaaf
Yevhen Nahalskyi <ltclmb.dev@gmail.com>
2024-08-04 03:34:06 +0200

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center p-24 w-full">
-      <h1 className="text-8xl font-semibold">ltclmb.dev</h1>
-      <h3 className="text-4xl mt-8">is in development 👨🏻‍💻</h3>
+      <h1 className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-semibold">
+        ltclmb.dev
+      </h1>
+      <h3 className="text-xl md:text-2xl lg:text-4xl mt-8">
+        is in development 👨🏻‍💻
+      </h3>
     </main>
   )
 }
