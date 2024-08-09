8734611e3dd319989acaece0ed6bd4b14de761dc
Yevhen Nahalskyi <ltclmb.dev@gmail.com>
2024-08-09 12:07:35 +0200

    update favicons

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -45,7 +45,6 @@ export default async function PostPage({
   params: { slug: string }
 }) {
   const post = await getPost(params.slug)
-  console.log('post', post)
 
   return (
     <div className="container">
