32d830388571bf59ddd3dacbda0199f2839219eb
Yevhen Nahalskyi <ltclmb.dev@gmail.com>
2024-08-20 13:38:00 +0200

    add generateMetadata

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,42 @@
 import React from 'react'
 import { getAllPosts, getPostBySlug } from '@/lib/posts'
 import PostTemplate from '@/templates/post-template'
+import { Metadata } from 'next'
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string }
+}): Promise<Metadata> {
+  const post = await getPostBySlug(params.slug)
+
+  return {
+    title: post.title,
+    description: post.description,
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      type: 'article',
+      url: `${process.env.NEXT_PUBLIC_URL}/posts/${params.slug}`,
+      images: [
+        {
+          url: `${process.env.NEXT_PUBLIC_URL}/posts/${params.slug}/opengraph-image`,
+          width: 1200,
+          height: 630,
+          alt: post.title,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: post.title,
+      description: post.description,
+      images: [
+        `${process.env.NEXT_PUBLIC_URL}/posts/${params.slug}/opengraph-image`,
+      ],
+    },
+  }
+}
 
 export default async function PostPage({
   params,
