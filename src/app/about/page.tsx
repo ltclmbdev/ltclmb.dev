4a8079123200a6aa5e7ff39036ba83ecc1a22931
Yevhen Nahalskyi <ltclmb.dev@gmail.com>
2024-08-20 16:05:33 +0200

    fix copy

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ I am Yevhen Nahalskyi, and I currently live in the beautiful city of Ljubljana, 
 
 I'm a front-end developer with over a decade of experience, specializing in JavaScript/TypeScript, ReactJS, NextJS, and CSS frameworks. I began my web development career in 2012 as a freelancer on [Upwork](https://www.upwork.com/freelancers/evgeniynagalskiy), where I completed numerous tasks and projects (over 100 contracts) and earned many satisfied clients.
 
-From 2015 until recently, I worked at [egghead.io](https://egghead.io/) and [Skill Recordings Inc.](https://www.skillrecordings.com/), where I was involved in developing cutting-edge e-learning products. My role involved creating seamless user experiences using JavaScript/TypeScript, ReactJS, NextJS, TailwindCSS, shadcn/ui, and the CMS Sanity.io.
+From 2015 until recently, I worked at [egghead.io](https://egghead.io/) and [Skill Recordings](https://www.skillrecordings.com/), where I was involved in developing cutting-edge e-learning products. My role involved creating seamless user experiences using JavaScript/TypeScript, ReactJS, NextJS, TailwindCSS, shadcn/ui, and the CMS Sanity.io.
 
 I am committed to continuous learning and stay updated with industry trends through platforms like Udemy and Coursera.
 
