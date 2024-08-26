9ed8dc282845258e29bdc33d3420ac3dc848c9fa
Yevhen Nahalskyi <ltclmb.dev@gmail.com>
2024-08-26 15:09:58 +0200

    fix font sizes

--- src/components/playground/ios-calculator/utils/get-font-size-class.ts
+++ src/components/playground/ios-calculator/utils/get-font-size-class.ts
@@ -5,10 +5,10 @@ export const getFontSizeClass = (display: string): string => {
 
   if (digitCount <= 4) return 'text-[64px]'
   if (digitCount === 5) return isNegative ? 'text-[61px]' : 'text-[64px]'
-  if (digitCount === 6) return isNegative ? 'text-[52px]' : 'text-[60px]'
+  if (digitCount === 6) return isNegative ? 'text-[50px]' : 'text-[60px]'
   if (digitCount === 7) return isNegative ? 'text-[44px]' : 'text-[50px]'
   if (digitCount === 8) return isNegative ? 'text-[39px]' : 'text-[44px]'
-  if (digitCount === 9) return isNegative ? 'text-[36px]' : 'text-[40px]'
+  if (digitCount === 9) return isNegative ? 'text-[35px]' : 'text-[40px]'
 
   return 'text-[40px]'
 }
